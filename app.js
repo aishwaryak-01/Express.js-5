b157d69b579f779c27d55bfec2cd94d90c9026f3
aishwaryak-01 <133705983+aishwaryak-01@users.noreply.github.com>
2023-10-04 08:58:06 +0530

    app.js

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-const express=require('express');
-const path=require('path');
-const bodyParser=require('body-parser');
-const app=express();
-const adminRouter=require('./routes/admin');
-const shopRouter=require('./routes/shop');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 const contactRouter=require('./routes/contactus');
 const successRouter=require('./routes/success');
+
+const app = express();
+
 app.use(bodyParser.urlencoded({extended:false}));
-app.use('/admin', adminRouter);
-app.use(shopRouter);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 app.use(contactRouter);
 app.use(successRouter);
-app.use( (request, response, next) => {
-    response.status(404).setHeader('Content-Type', 'text/html').sendFile(path.join(__dirname, 'views', '404.html'));
-});
+
+app.use('/', (req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
+
 app.listen(3000);
+
+
